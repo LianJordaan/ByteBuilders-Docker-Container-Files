752571b8abe7d1961e583960ca502c9b16b55bda
LianJordaan <lianjordaan30052005@gmail.com>
2024-09-23 22:31:36 +0200

    Addes support for databases to now use passwords to prevent it from getting hacked!

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,9 @@ export $(grep -v '^#' /.env | xargs)
 # Replace placeholder with actual secret in paper-global.yml
 sed -i "s/YOUR_SECRET_HERE/${VELOCITY_SECRET}/g" /minecraft/config/paper-global.yml
 
+# Replace placeholder with actual password in LuckPerms/config.yml
+sed -i "s/YOUR_DATABASE_PASSWORD_HERE/${YOUR_DATABASE_PASSWORD_HERE}/g" /minecraft/plugins/LuckPerms/config.yml
+
 # Set server-port in server.properties
 echo "server-port=${PORT}" >> /minecraft/server.properties
 
